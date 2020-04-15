8d709963f677fe84cada3cbfd816e1b62fe8d2b4
Mahmoud Swehli <mahmoud@moodio.co.uk>
2020-04-15 16:26:13 +0100

    fixed jpg signature

--- Moodio.Common/Utils/FileHelpers.cs
+++ Moodio.Common/Utils/FileHelpers.cs
@@ -17,15 +17,19 @@ namespace Moodio.Utils
                 "jpeg", new List<byte[]>()
                 {
                     new byte[]{ 0xFF, 0xD8, 0xFF, 0xE0 },
+                    new byte[]{ 0xFF, 0xD8, 0xFF, 0xE1 },
                     new byte[]{ 0xFF, 0xD8, 0xFF, 0xE2 },
-                    new byte[]{ 0xFF, 0xD8, 0xFF, 0xE3 }
+                    new byte[]{ 0xFF, 0xD8, 0xFF, 0xE3 },
+                    new byte[]{ 0xFF, 0xD8, 0xFF, 0xE8 }
                 }
             },
             {
                 "jpg", new List<byte[]>()
                 {
                     new byte[]{ 0xFF, 0xD8, 0xFF, 0xE0 },
+                    new byte[]{ 0xFF, 0xD8, 0xFF, 0xE1 },
                     new byte[]{ 0xFF, 0xD8, 0xFF, 0xE2 },
+                    new byte[]{ 0xFF, 0xD8, 0xFF, 0xE3 },
                     new byte[]{ 0xFF, 0xD8, 0xFF, 0xE8 }
                 }
             },
